f6b770077d5c541f3bbfb21f04bf1e139ce30feb
root <root@LAPTOP-JK3OUFM2.localdomain>
2023-02-10 15:19:16 +0700

    implement testcase for nonce and difficulty number of block

--- block.test.js
+++ block.test.js
@@ -7,13 +7,16 @@ describe('Block', () => {
     const lastHash = "foo-hash";
     const hash = "bar-hash";
     const data = ['blockchain', 'data'];
-    const block = new Block({ timestamp, lastHash, hash, data });
-
-    it('Has a timestamp, lastHash, has and data property', () => {
+    const nonce = 1;
+    const difficulty = 1;
+    const block = new Block({ timestamp, lastHash, hash, data, nonce, difficulty });
+    it('Has a timestamp, lastHash, hash, data property, nonce\'s value and difficulty\'s value', () => {
         expect(block.timestamp).toEqual(timestamp);
         expect(block.lastHash).toEqual(lastHash);
         expect(block.hash).toEqual(hash);
         expect(block.data).toEqual(data);
+        expect(block.nonce).toEqual(nonce);
+        expect(block.difficulty).toEqual(difficulty);
     })
     describe('genesis()', () => {
         const genesisBlock = Block.genesis();
@@ -27,25 +30,37 @@ describe('Block', () => {
     describe('mineBlock()', () => {
         const lastBlock = Block.genesis();
         const data = 'mined data'
-        const minedBlock = Block.mineBlock({lastBlock, data})
-        
-        it('returns a Block instances', ()=>{
+        const minedBlock = Block.mineBlock({ lastBlock, data })
+
+        it('returns a Block instances', () => {
             expect(minedBlock instanceof Block).toBe(true);
         })
-        it('set the `lastHash` to be the `hash` of lastBlock', ()=> {
+        it('set the `lastHash` to be the `hash` of lastBlock', () => {
             expect(minedBlock.lastHash).toEqual(lastBlock.hash);
         })
-        it('set the `data`', ()=> {
+        it('set the `data`', () => {
             expect(minedBlock.data).toEqual(data);
         })
-        it('set a `timestamp`',()=>{
+        it('set a `timestamp`', () => {
             expect(minedBlock.timestamp).not.toEqual(undefined)
         })
-        it('creates a SHA-256 `hash` based on the proper inputs', ()=>{
+        it('creates a SHA-256 `hash` based on the proper inputs', () => {
             expect(minedBlock.hash)
-                .toEqual(cryptoHash(minedBlock.timestamp, lastBlock.hash, data))
+                .toEqual(
+                    cryptoHash(
+                        minedBlock.timestamp,
+                        minedBlock.nonce,
+                        minedBlock.difficulty,
+                        lastBlock.hash,
+                        data
+                    )
+                )
+        });
+        it('set the `hash` that matches the difficulty criteria', () => {
+            expect(minedBlock.hash.substring(0, minedBlock.difficulty))
+                .toEqual('0'.repeat(minedBlock.difficulty));
         });
     });
-    
+
 });
 
