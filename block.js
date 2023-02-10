2b06172c2ef243d946ae84548d34df9e32083b94
HuyHuynh3103 <19127420@student.hcmus.edu.vn>
2023-02-10 16:46:44 +0700

    difficulty should not be lower than 1

--- block.js
+++ block.js
@@ -30,8 +30,11 @@ class Block {
     }
     static adjustDifficulty({ originalBlock, timestamp }) {
         const { difficulty } = originalBlock;
+        if(difficulty < 1) return 1;
+
         const difference = timestamp - originalBlock.timestamp;
         if (difference > MINE_RATE) return difficulty - 1;
+        
         return difficulty + 1;
     }
 }
--- block.test.js
+++ block.test.js
@@ -79,6 +79,10 @@ describe('Block', () => {
                 timestamp: block.timestamp + MINE_RATE + 100
             })).toEqual(block.difficulty - 1);
         })
+        it('has lower limit of 1', ()=>{
+            block.difficulty = -1;
+            expect(Block.adjustDifficulty({originalBlock: block})).toEqual(1)
+        })
     });
 
 
