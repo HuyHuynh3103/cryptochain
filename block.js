b185bbc7501a87b589fd96d0fbda0e7282a4bfc9
HuyHuynh3103 <19127420@student.hcmus.edu.vn>
2023-02-10 16:22:10 +0700

    Implement adjust the difficulty in order to the mining time arounds MINE_RATE

--- block.js
+++ block.js
@@ -1,4 +1,4 @@
-const { GENESIS_DATA } = require("./config");
+const { GENESIS_DATA, MINE_RATE } = require("./config");
 const cryptoHash = require("./crypto-hash");
 
 class Block {
@@ -26,6 +26,12 @@ class Block {
         } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty))
 
         return new this({ timestamp, lastHash, hash, data, nonce, difficulty });
+    }
+    static adjustDifficulty({originalBlock, timestamp}){
+        const {difficulty} = originalBlock;
+        const difference = timestamp - originalBlock.timestamp;
+        if(difference > MINE_RATE) return difficulty - 1;
+        return difficulty - 1;
     }
 }
 module.exports = Block;
