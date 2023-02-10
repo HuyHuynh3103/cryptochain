69b2e649d2620f908742aa31720fb8c1e150c3c6
root <root@LAPTOP-JK3OUFM2.localdomain>
2023-02-10 16:04:34 +0700

    implement nonce and static difficulty value of the block

--- block.js
+++ block.js
@@ -2,20 +2,30 @@ const { GENESIS_DATA } = require("./config");
 const cryptoHash = require("./crypto-hash");
 
 class Block {
-    constructor({ timestamp, lastHash, hash, data }) {
+    constructor({ timestamp, lastHash, hash, data, nonce, difficulty }) {
         this.timestamp = timestamp;
         this.lastHash = lastHash;
         this.hash = hash;
         this.data = data;
+        this.nonce = nonce;
+        this.difficulty = difficulty;
     }
     static genesis() {
         return new this(GENESIS_DATA);
     }
     static mineBlock({ lastBlock, data }) {
-        const timestamp = Date.now();
+        let hash, timestamp;
         const lastHash = lastBlock.hash;
-        const hash = cryptoHash(timestamp, lastHash, data)
-        return new this({ timestamp, lastHash, hash, data });
+        const difficulty = lastBlock.difficulty;
+        let nonce = 0; // to be able to adjust
+
+        do {
+            nonce++;
+            timestamp = Date.now();
+            hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty)
+        } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty))
+
+        return new this({ timestamp, lastHash, hash, data, nonce, difficulty });
     }
 }
 module.exports = Block;
--- blockchain.js
+++ blockchain.js
@@ -19,12 +19,12 @@ class Blockchain {
         }
         for (let i = 1; i < chain.length; i++) {
             const block = chain[i];
+            const { timestamp, lastHash, hash, data, nonce, difficulty } = block;
 
             const actualLastHash = chain[i - 1].hash;
 
-            const { timestamp, lastHash, hash, data } = block;
             if (lastHash !== actualLastHash) return false;
-            const validatedHash = cryptoHash(timestamp, lastHash, data);
+            const validatedHash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
             if (hash !== validatedHash) return false;
         }
         return true;
