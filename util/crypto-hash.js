c6fbbb466cd888e09bd0c5c1b281531031ae9379
HuyHuynh3103 <19127420@student.hcmus.edu.vn>
2023-02-12 01:13:32 +0700

    Update cryptoHash function

--- util/crypto-hash.js
+++ util/crypto-hash.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto');
 const cryptoHash = (...args) => {
     const hash = crypto.createHash('sha256');
-    hash.update(args.sort().join(' '));
+    hash.update(args.map(arg => JSON.stringify(arg)).sort().join(' '));
 
     // represent the result of a hash as hexadecimal form
     return hash.digest('hex');
