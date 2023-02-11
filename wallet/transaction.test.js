005bbad122fc52c0a0652dc6fd89404fb5059310
HuyHuynh3103 <19127420@student.hcmus.edu.vn>
2023-02-12 00:05:17 +0700

    create input for transaction both implementation and testing

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -1,5 +1,7 @@
 const Transaction = require('./transaction');
 const Wallet = require('./index');
+const { verifySignature } = require('../util');
+
 describe('Transaction', () => {
     let transaction, senderWallet, recipient, amount;
 
@@ -24,7 +26,28 @@ describe('Transaction', () => {
         });
 
         it('outputs the remaining balance for the `senderWallet`', () => {
-            expect(transaction.outputMap[senderWallet.publicKey ]).toEqual(senderWallet.balance - amount);
+            expect(transaction.outputMap[senderWallet.publicKey]).toEqual(senderWallet.balance - amount);
+        })
+    });
+    describe('input', () => {
+        it('has an `input` property', () => {
+            expect(transaction).toHaveProperty('input');
+        });
+        it('has a `timestamp` in the input', ()=>{
+            expect(transaction.input).toHaveProperty('timestamp');
+        });
+        it('sets the `amount` in input to the `senderWallet` balance', ()=>{
+            expect(transaction.input.amount).toEqual(senderWallet.balance); 
+        });
+        it('sets the `address` in input to the `senderWallet` public key', () => {
+            expect(transaction.input.address).toEqual(senderWallet.publicKey);
+        });
+        it('signs the input', () => {
+            expect(verifySignature({
+                publicKey: senderWallet.publicKey,
+                data: transaction.outputMap,
+                signature: transaction.input.signature
+            })).toBe(true);
         })
     });
 
